fix some vanishing gradient stuff

bandit-layer/bandit_layer_FMNIST.py
@@ -24,8 +24,10 @@ class BanditLayer(nn.Module):
         nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, input):
-        input = input.view(input.size(0), -1)  # Flatten the input tensor
-
+        if not self.training:
+            return torch.matmul(input, self.weight.t()) + self.bias
+        
+        # input = input.view(input.size(0), -1)  # Flatten the input tensor
         m, n = input.size(0), self.weight.size(0)
         p = input.size(1)
 
@@ -34,7 +36,7 @@ class BanditLayer(nn.Module):
         for j in range(n):
           query_vector = self.weight[j]
           k = math.ceil(m/3)
-          ApproximateActiveSet,_ = BoundedME(input, query_vector, k, 0.1,0.1)
+          ApproximateActiveSet,_ = BoundedME(input, query_vector, k, 0.3,0.1)
           for i in ApproximateActiveSet:
             output[i, j] = torch.dot(input[i], self.weight[j]) + self.bias[j]
 
@@ -45,14 +47,17 @@ class MyModel(nn.Module):
         super(MyModel, self).__init__()
 
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.bn2d = nn.BatchNorm2d(16,affine=True)
+        # batch norm is needed to reduce vanishing gradient. 
         self.relu = nn.ReLU()
-        self.fc = BanditLayer(16 * 28 * 28, 10)
+        self.fc1 = BanditLayer(16 * 28 * 28, 10)
 
     def forward(self, x):
         x = self.conv1(x)
+        #x = self.bn2d(x)
         x = self.relu(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc1(x)
         return x
 
 # Load the FashionMNIST dataset
@@ -73,7 +78,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # Training loop
-num_epochs = 5
+num_epochs = 1
 for epoch in range(num_epochs):
     running_loss = 0.0
     for i, (inputs, labels) in enumerate(train_loader):

bandit-layer/torchBME.py
@@ -3,27 +3,43 @@ import torch.nn.functional as F
 import math
 import numpy as np
 
-def compute_m(delta, epsilon, A_arms):
-    b = torch.max(A_arms, dim=0).values
-    a = torch.zeros_like(b)  # torch.min(A_arms, dim=0).values
+MIN_FLOAT = 2 ** (-300)
 
-    u = ((np.log(1/delta) / 2) * ((b - a) ** 2) / (epsilon ** 2))[0]
+def compute_m(delta, epsilon, A_arms):
+    # in pytorch dim = 1 is column wise. dim = 1 is row wise
+    b = torch.max(A_arms, dim=1).values[0]
+    a = torch.min(A_arms, dim=1).values[0]
+    
+    u = ((np.log(1/delta) / 2) * ((b - a) ** 2) / (epsilon ** 2))
     m = min((u + 1) / (1 + len(A_arms) * u), (u + u) / (1 + len(A_arms) * u))
     return m
 
-
-def pull_number_modifier(current_epsilon, current_delta, S_size, K):
-    epsilon_coefficient = 2 / (current_epsilon ** 2)
-    log_term_numerator = 2 * (S_size - K)
-    log_term_denominator = current_delta * ((S_size - K) / 2 + 1)
-    return epsilon_coefficient * math.log(log_term_numerator / log_term_denominator)
-
 def remove_low_means(S, mean_dict, K):
-    threshold_margin = math.ceil((len(S)-K) / 2)
+    elemnts_to_remove = math.ceil((len(S)-K) / 2)
     sorted_mean_list = sorted(mean_dict.items(), key=lambda x: x[1], reverse=False)
-    threshold_mean = sorted_mean_list[threshold_margin][1]
+    threshold_mean = sorted_mean_list[elemnts_to_remove][1]
     new_mean_dict = dict(filter(lambda x: x[1] >= threshold_mean, mean_dict.items()))
     return new_mean_dict
+
+def pull_number_modifier(current_epsilon, current_delta, S_size, K):
+    epsilon_coefficient = 2 / max(MIN_FLOAT,(current_epsilon ** 2))
+    log_term_numerator = 2 * (S_size - K)
+    log_term_denominator = max(MIN_FLOAT, current_delta * ((S_size - K) / 2 + 1))
+    return epsilon_coefficient * math.log(log_term_numerator / log_term_denominator)
+
+def pull_new_arms(query_vector, A_arms, K, S_current, reward_dictionary, old_t, new_t):
+    ops = 0
+    for i in S_current:
+        a = A_arms[i]
+        arm_pulls = torch.dot(query_vector[old_t:new_t], a[old_t:new_t])
+        reward = arm_pulls  # / (new_t - old_t - 1) # no need for actual mean
+        ops += query_vector[old_t:new_t].numel()  # Counting multiplications inside torch.dot
+
+        reward_dictionary[i] += reward
+
+    reward_dictionary = remove_low_means(S_current, reward_dictionary, K)
+    ops += 2
+    return reward_dictionary, ops
 
 # Bounded Median Estimator
 def BME( A_arms, query_vector, K, epsilon=0.1, delta=0.3):
@@ -36,9 +52,8 @@ def BME( A_arms, query_vector, K, epsilon=0.1, delta=0.3):
 
     m = compute_m(delta, epsilon, A_arms)
     old_t = 0
-    S_previous = None
     while len(S_current) > K:
-        new_t = math.floor(m * pull_number_modifier(current_epsilon, current_delta, len(S_current), K))
+        new_t = math.ceil(max(MIN_FLOAT, m * pull_number_modifier(current_epsilon, current_delta, len(S_current), K)))
         op_count += 6
 
         reward_dictionary, ops = pull_new_arms(query_vector, A_arms, K, S_current, reward_dictionary, old_t, new_t)
@@ -49,23 +64,6 @@ def BME( A_arms, query_vector, K, epsilon=0.1, delta=0.3):
         current_epsilon = current_epsilon * 3/4
         current_delta = current_delta / 2
         old_t = new_t
-        S_previous = S_current
         op_count += 3
 
     return S_current, op_count
-
-def pull_new_arms(query_vector, A_arms, K, S_current, reward_dictionary, old_t, new_t):
-    ops = 0
-    for i in S_current:
-        a = A_arms[i]
-        #print(torch.tensor(a)[old_t:new_t])
-        arm_pulls = torch.dot(query_vector[old_t:new_t], a[old_t:new_t])
-        reward = arm_pulls  # / (new_t - old_t - 1) # no need for actual mean
-        ops += query_vector[old_t:new_t].numel()  # Counting multiplications inside torch.dot
-
-        reward_dictionary[i] += reward
-
-    reward_dictionary = remove_low_means(S_current, reward_dictionary, K)
-    ops += 2
-    return reward_dictionary, ops
-